0f7946819ca8abf7985e527efccc2d65eb949eff
Mirari1 <kir1997@inbox.ru>
2017-07-04 23:42:52 +0300

    first commit

---
+++ Bootstrap 3/js/script1.js
@@ -0,0 +1,27 @@
+function foo(){
+	document.getElementById('1').value = "18 руб"
+}
+function foo1(){
+	document.getElementById('1').value = "20 руб"
+}
+function fo(){
+	document.getElementById('1').value = "23 руб"
+}
+function fol(){
+	document.getElementById('1').value = "28 руб"
+}
+function foa(){
+	document.getElementById('1').value = "34 руб"
+}
+function flo(){
+	document.getElementById('1').value = "48 руб"
+}
+function fla(){
+	document.getElementById('1').value = "43 руб"
+}
+function fll(){
+	document.getElementById('1').value = "54 руб"
+}
+function fok(){
+	document.getElementById('1').value = "80 руб"
+}
